188a9067fa8bf3ace6731f50f339244e53ad0625
taylor <taylor.arnold@acm.org>
2017-06-27 18:38:31 +0100

    attempting to fix path issue on Windows

--- R/annotate.R
+++ R/annotate.R
@@ -103,6 +103,7 @@ run_annotators <- function(input, file = NULL, output_dir = NULL, load = TRUE,
     stop("base of the file argument does not point to a known directory")
 
   output_dir <- file.path(Sys.glob(output_dir), "")
+  output_dir <- gsub("\\", "/", output_dir, fixed = TRUE)
 
   if (as_strings) {
     new_input <- NULL
