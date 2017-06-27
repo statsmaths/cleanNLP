728db23457b15b27f07830dfcbd513063dace58c
taylor <taylor.arnold@acm.org>
2017-06-27 18:52:59 +0100

    update tests for new path formula

--- tests/testthat/test-tokenizers.R
+++ tests/testthat/test-tokenizers.R
@@ -74,6 +74,7 @@ test_that("run_annotators options", {
   anno <- run_annotators(input_files, output_dir = od, load = FALSE,
     backend = "tokenizers")
   od <- file.path(Sys.glob(od), "")
+  od <- gsub("\\", "/", od, fixed = TRUE)
   expect_equal(anno, od)
 })
 
