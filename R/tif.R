9753a71078c37b9a707eca42e0ddaab82a8fdbb0
Taylor Arnold <taylor.arnold@acm.org>
2018-03-13 03:51:39 +0000

    Update tif.R

--- R/tif.R
+++ R/tif.R
@@ -4,15 +4,22 @@
 #' a one table, denormalized version of the annotation object.
 #'
 #' @param annotation    an annotation object
+#' @param rename        logical. Should columns be renamed to match
+#'                      the text interchange format.
 #'
 #' @author Taylor B. Arnold, \email{taylor.arnold@@acm.org}
 #' @export
-cnlp_get_tif <- function(annotation) {
+cnlp_get_tif <- function(annotation, rename=TRUE) {
 
-  cnlp_get_token(annotation, include_root = FALSE,
-            combine = TRUE, remove_na = TRUE,
-            spaces = TRUE)
+  output <- cnlp_get_token(annotation, include_root = FALSE,
+                           combine = TRUE, remove_na = TRUE,
+                           spaces = TRUE)
 
+  if (rename) {
+    names(output)[c(1L, 4L)] <- c("doc_id", "token") 
+  }
+  
+  return(output)
 }
 
 
